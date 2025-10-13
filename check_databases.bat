6b40314b6ed17df929570a86a65f8c8df12b45e8
GiljoAi <infoteam@giljo.ai>
2025-10-13 19:33:50 -0400

    working through handovers, cleaned up root folder of obsolte items and moved some docs

--- check_databases.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set PGPASSWORD=4010
-echo Checking for giljo databases...
-"C:\Program Files\PostgreSQL\18\bin\psql.exe" -U postgres -t -c "SELECT datname FROM pg_database WHERE datname LIKE 'giljo%%';"
-echo.
-echo Done checking databases.
